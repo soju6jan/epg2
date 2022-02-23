b1f9983c42949b4a883251ed085a3619317ebe1f
soju6jan <cybersol@naver.com>
2022-02-24 01:21:52 +0900

    update

--- file/epg_upload.sh
+++ file/epg_upload.sh
@@ -2,9 +2,9 @@
 export LANG=en_US.utf8
 NowDate=$(date +%Y%m%d)-$(date +%H%M)
 PLUGIN_HOME=$1
-cd $PLUGIN_HOME && 
-git --git-dir=$PLUGIN_HOME/.git pull &&
-git --git-dir=$PLUGIN_HOME/.git add $PLUGIN_HOME/file/epg2_data.db && 
-git --git-dir=$PLUGIN_HOME/.git add $PLUGIN_HOME/file/xmltv_all2.xml
-git --git-dir=$PLUGIN_HOME/.git commit -m $NowDate && 
+cd $PLUGIN_HOME; 
+git --git-dir=$PLUGIN_HOME/.git pull;
+git --git-dir=$PLUGIN_HOME/.git add $PLUGIN_HOME/file/epg2_data.db;
+git --git-dir=$PLUGIN_HOME/.git add $PLUGIN_HOME/file/xmltv_all2.xml;
+git --git-dir=$PLUGIN_HOME/.git commit -m $NowDate;
 git --git-dir=$PLUGIN_HOME/.git push
