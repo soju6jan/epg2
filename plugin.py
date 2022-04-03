264082183da8cbdaa1cbe032f3427429c77f261a
soju6jan <cybersol@naver.com>
2022-04-03 20:42:59 +0900

    maker not load

--- plugin.py
+++ plugin.py
@@ -55,17 +55,18 @@ ModelSetting = P.ModelSetting
 
 def initialize():
     try:
-        if os.path.exists(os.path.join(os.path.dirname(__file__), 'file', 'cred')) == False:
-            del P.menu['sub'][1]
-
         app.config['SQLALCHEMY_BINDS'][P.package_name] = 'sqlite:///%s' % (os.path.join(path_data, 'db', '{package_name}.db'.format(package_name=P.package_name)))
         app.config['SQLALCHEMY_BINDS'][EPG_DATA_DB_BIND_KEY] = 'sqlite:///%s' % (os.path.join(os.path.dirname(__file__), 'file', f'{EPG_DATA_DB_BIND_KEY}.db'))
 
         PluginUtil.make_info_json(P.plugin_info, __file__)
-
+        
         from .logic_user import LogicUser
-        from .logic_maker import LogicMaker
-        P.module_list = [LogicUser(P), LogicMaker(P)]
+        P.module_list = [LogicUser(P)]
+        if os.path.exists(os.path.join(os.path.dirname(__file__), 'file', 'cred')) == False:
+            del P.menu['sub'][1]
+        else:
+            from .logic_maker import LogicMaker
+            P.module_list.append(LogicMaker(P))
         P.logic = Logic(P)
         default_route(P)
     except Exception as e: 
